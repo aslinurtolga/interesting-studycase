62a05f4756ee19051760b232574ac0662f3dece7
AslinurTolga <aslinurtolga@gmail.com>
2023-07-18 21:30:58 +0300

    feat: useState and useEffect defined

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const [posts, setPosts] = useState([]);
 
-export default HomePage
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.log("Error fetching posts:", error);
+      }
+    };
+    fetchPosts();
+  }, []);
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
