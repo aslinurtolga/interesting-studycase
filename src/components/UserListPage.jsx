5d958b2893824be8ba313db06871db2e3e067247
AslinurTolga <aslinurtolga@gmail.com>
2023-07-19 01:04:46 +0300

    feat: users brought

--- src/components/UserListPage.jsx
+++ src/components/UserListPage.jsx
@@ -4,6 +4,30 @@ const UserListPage = () => {
   // state definitions
   const [user, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Function that brings users
+    const getUsers = async () => {
+    try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const userData = await response.json();
+    setUsers(userData);
+    setLoading(false);
+    } catch (error) {
+    console.log("Error fetching users:", error);
+    }
+  };
+
+
+
+
+
+  })
+
+
+
+
+
   return <div>UserListPage</div>;
 }
 
