52d48841ffd55d2ee9d6af313c974dc720efd428
AslinurTolga <aslinurtolga@gmail.com>
2023-07-19 00:50:57 +0300

    feat: if the post is loaded, its details are shown

--- src/components/PostDetailPage.jsx
+++ src/components/PostDetailPage.jsx
@@ -42,7 +42,25 @@ const PostDetailPage = () => {
     return <div>Loading...</div>;
   }
 
-  return <div>PostDetailPage</div>;
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
+      <p className="text-lg mb-4">{post.body}</p>
+
+      <h2 className="text-2xl font-semibold mb-2">Comments</h2>
+      <div className="grid grid-cols-1 gap-4">
+        {comments.map((comment) => (
+          <div
+            key={comment.id}
+            className="bg-gray-100 p-4 rounded-lg shadow-md"
+          >
+            <h3 className="text-xl font-semibold mb-2">{comment.name}</h3>
+            <p>{comment.body}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PostDetailPage;
