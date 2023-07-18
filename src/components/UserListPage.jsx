9c91b6a059d21bb7c8bd17cb50c008ed0d791375
AslinurTolga <aslinurtolga@gmail.com>
2023-07-19 01:07:50 +0300

    feat: show loading on loading status

--- src/components/UserListPage.jsx
+++ src/components/UserListPage.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
 const UserListPage = () => {
   // state definitions
@@ -6,29 +6,30 @@ const UserListPage = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Function that brings users
+    // function that brings users
     const getUsers = async () => {
-    try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const userData = await response.json();
-    setUsers(userData);
-    setLoading(false);
-    } catch (error) {
-    console.log("Error fetching users:", error);
-    }
-  };
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const userData = await response.json();
+        setUsers(userData);
+        setLoading(false);
+      } catch (error) {
+        console.log("Error fetching users:", error);
+      }
+    };
+    // calling the fetch users function
+    getUsers();
+  }, []);
 
-
-
-
-
-  })
-
-
-
+  if (loading) {
+    // if it is in the loading state
+    return <div>Loading...</div>;
+  }
 
 
   return <div>UserListPage</div>;
-}
+};
 
-export default UserListPage
+export default UserListPage;
