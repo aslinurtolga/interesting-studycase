264d3dd4cdc9a4f3763d02c4390f8ed876c16bf5
AslinurTolga <aslinurtolga@gmail.com>
2023-07-19 00:01:38 +0300

    feat: given redirect function using onClick

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,9 @@
-import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 const HomePage = () => {
   const [posts, setPosts] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -20,19 +21,25 @@ const HomePage = () => {
     fetchPosts();
   }, []);
 
+  const handlePostClick = (postId) => {
+    router.push(`/post/${postId}`);
+  };
+
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold text-blue-500 mb-4">Posts</h1>
       <div className="grid grid-cols-1 gap-4">
         {posts.map((post) => (
-          <div key={post.id} className="bg-gray-100 p-4 rounded-lg shadow-md">
+          <div
+            key={post.id}
+            className="bg-gray-100 p-4 rounded-lg shadow-md cursor-pointer"
+            onClick={() => handlePostClick(post.id)}
+          >
             <h2 className="text-2xl font-semibold text-blue-800">
               {post.title}
             </h2>
             <p className="text-[#A76F6F]">{post.body}</p>
-            <Link href={`/post/${post.id}`}>
-              <a>View Details</a>
-            </Link>
+            <p className="text-blue-500 underline">View Details</p>
           </div>
         ))}
       </div>
