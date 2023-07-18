d54b148291e6353268ab527e648c249b3876ef7b
AslinurTolga <aslinurtolga@gmail.com>
2023-07-19 01:17:15 +0300

    feat: users pressed to screen with map function

--- src/components/UserListPage.jsx
+++ src/components/UserListPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const UserListPage = () => {
   // state definitions
-  const [user, setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -28,8 +28,27 @@ const UserListPage = () => {
     return <div>Loading...</div>;
   }
 
+  // if users are loaded
 
-  return <div>UserListPage</div>;
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="flex justify-center text-4xl font-bold text-blue-500 mb-4">
+        Users
+      </h1>
+      {users.map((user) => (
+        <div
+          key={user.id}
+          className="mb-4 bg-gray-100 p-4 rounded-lg shadow-md"
+        >
+          <h3 className="text-2xl font-semibold text-purple-700">
+            {user.name}
+          </h3>
+          <p className="text-pink-800">Email: {user.email}</p>
+          <p className="text-green-500">City: {user.address.city}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default UserListPage;
