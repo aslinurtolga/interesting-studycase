473873e6af165e3acda7ca1c4bc1fa2166df61f7
AslinurTolga <aslinurtolga@gmail.com>
2023-07-18 21:39:19 +0300

    feat: data was mapped and displayed

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 const HomePage = () => {
   const [posts, setPosts] = useState([]);
@@ -15,9 +16,28 @@ const HomePage = () => {
         console.log("Error fetching posts:", error);
       }
     };
+
     fetchPosts();
   }, []);
-  return <div>HomePage</div>;
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold text-blue-500 mb-4">Posts</h1>
+      <div className="grid grid-cols-1 gap-4">
+        {posts.map((post) => (
+          <div key={post.id} className="bg-gray-100 p-4 rounded-lg shadow-md">
+            <h2 className="text-2xl font-semibold text-blue-800">
+              {post.title}
+            </h2>
+            <p className="text-[#A76F6F]">{post.body}</p>
+            <Link href={`/post/${post.id}`}>
+              <a>View Details</a>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
