83a014810acc6bcc667e268be67fecf62a8e0e53
AslinurTolga <aslinurtolga@gmail.com>
2023-07-19 02:01:19 +0300

    feat: get albums from api, state updated

--- src/components/UserDetailPage.jsx
+++ src/components/UserDetailPage.jsx
@@ -1,14 +1,29 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 
-const UserDetailPage = ({userId}) => {
+const UserDetailPage = ({ userId }) => {
   // Define states to monitor albums and upload status
   const [albums, setAlbums] = useState([]);
-  cons [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // Define the function that brings the albums
-  })
+    const fetchAlbums = async () => {
+      try {
+        // fetch albums from API
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/albums?userId=${userId}`
+        );
+        const albumData = await response.json();
+        // Sets albums to state and updates the upload status
+        setAlbums(albumData);
+        setLoading(false);
+      } catch (error) {
+        console.log("Error fetching albums: ", error);
+      }
+    };
+    // using the useEffect hook, the fetchAlbums function is executed when the component is loaded
+  });
 
   return <div>UserDetailPage</div>;
 };
