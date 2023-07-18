fdc12e21745de7ff4d0853112d37880a23599cde
AslinurTolga <aslinurtolga@gmail.com>
2023-07-19 00:33:54 +0300

    feat: pulled comment data with useEffect

--- src/components/PostDetailPage.jsx
+++ src/components/PostDetailPage.jsx
@@ -3,20 +3,31 @@ import React, { useEffect, useState } from "react";
 
 const PostDetailPage = () => {
   const router = useRouter();
-  const [post, setPost] = useState(null)
+  const [post, setPost] = useState(null);
+  const [comments, setComments] = useState([])
 
   useEffect(() => {
   const fetchPost = async () => {
-  try {
-  const response = await fetch (`https://jsonplaceholder.typicode.com/posts/${id}`);
-  
-  const postData = await response.json();
-  setPost(postData);
-  } catch (error) {
-  console.log("Error fetching post:", error);
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/posts/${id}`
+        );
+        const postData = await response.json();
+        setPost(postData);
+      } catch (error) {
+        console.log("Error fetching post:", error);
+      }
+    };
+
+    const fetchComments = async () => {
+    try {
+      const response = await fetch (`https://jsonplaceholder.typicode.com/posts/${id}/comments`);
+      const commentsData = await response.json();
+      setComments(commentsData);
+    } catch (error) {
+    console.log("Error fetching comments:", error);
   }
-  }
-  }
+    };
 
 
 
