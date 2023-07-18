b0278138c13796cd53aad82b4b9cf3a209e9e089
AslinurTolga <aslinurtolga@gmail.com>
2023-07-18 21:24:21 +0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import HomePage from '@/components/HomePage'
+import PostDetailPage from '@/components/PostDetailPage'
+import UserDetailPage from '@/components/UserDetailPage'
+import UserListPage from '@/components/UserListPage'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+   <>
+   <HomePage/>
+   <PostDetailPage/>
+   <UserDetailPage/>
+   <UserListPage/>
+   </>
+  )
+}
