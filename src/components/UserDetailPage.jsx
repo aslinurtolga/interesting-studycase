06a43c9a9686e958929127828a0ce8c89a021a1e
AslinurTolga <aslinurtolga@gmail.com>
2023-07-19 02:25:54 +0300

    feat: requests checks

--- src/components/UserDetailPage.jsx
+++ src/components/UserDetailPage.jsx
@@ -14,7 +14,9 @@ const UserDetailPage = ({ userId }) => {
         const response = await fetch(
           `https://jsonplaceholder.typicode.com/albums?userId=${userId}`
         );
+        console.log(response); // İsteğin yanıtını kontrol etmek için
         const albumData = await response.json();
+        console.log(albumData); // Alınan veriyi kontrol etmek için
         // Sets albums to state and updates the upload status
         setAlbums(albumData);
         setLoading(false);
@@ -31,7 +33,29 @@ const UserDetailPage = ({ userId }) => {
     return <div>Loading...</div>;
   }
 
-  return <div>UserDetailPage</div>;
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-4">User Albums</h1>
+      {albums.map((album) => (
+        <div key={album.id} className="mb-8">
+          <h3 className="text-2xl font-semibold mb-4">{album.title}</h3>
+          <div className="grid grid-cols-4 gap-4">
+            {album.photos.map((photo) => (
+              <div key={photo.id} className="rounded-lg overflow-hidden">
+                <Image
+                  src={photo.url}
+                  alt={photo.title}
+                  width={200}
+                  height={200}
+                />
+                <p className="text-center mt-2">{photo.title}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default UserDetailPage;
--- src/components/UserListPage.jsx
+++ src/components/UserListPage.jsx
@@ -1,52 +1,48 @@
 import React, { useEffect, useState } from "react";
+import UserDetailPage from "./UserDetailPage";
 
 const UserListPage = () => {
-  // state definitions
   const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [selectedUserId, setSelectedUserId] = useState(null);
 
   useEffect(() => {
-    // function that brings users
-    const getUsers = async () => {
+    const fetchUsers = async () => {
       try {
         const response = await fetch(
           "https://jsonplaceholder.typicode.com/users"
         );
         const userData = await response.json();
         setUsers(userData);
-        setLoading(false);
       } catch (error) {
         console.log("Error fetching users:", error);
       }
     };
-    // calling the fetch users function
-    getUsers();
+
+    fetchUsers();
   }, []);
 
-  if (loading) {
-    // if it is in the loading state
-    return <div>Loading...</div>;
-  }
-
-  // if users are loaded
+  const handleUserClick = (userId) => {
+    setSelectedUserId(userId);
+  };
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="flex justify-center text-4xl font-bold text-blue-500 mb-4">
-        Users
-      </h1>
-      {users.map((user) => (
-        <div
-          key={user.id}
-          className="mb-4 bg-gray-100 p-4 rounded-lg shadow-md"
-        >
-          <h3 className="text-2xl font-semibold text-purple-700">
-            {user.name}
-          </h3>
-          <p className="text-pink-800">Email: {user.email}</p>
-          <p className="text-green-500">City: {user.address.city}</p>
-        </div>
-      ))}
+      <h1 className="text-4xl font-bold mb-8">Users</h1>
+      <div className="grid grid-cols-3 gap-8">
+        {users.map((user) => (
+          <div
+            key={user.id}
+            className="bg-gray-100 p-4 rounded-lg shadow-md cursor-pointer"
+            onClick={() => handleUserClick(user.id)}
+          >
+            <h3 className="text-xl font-semibold mb-2">{user.name}</h3>
+            <p className="text-purple-700">Email: {user.email}</p>
+            <p className="text-green-500">City: {user.address.city}</p>
+          </div>
+        ))}
+      </div>
+
+      {selectedUserId && <UserDetailPage userId={selectedUserId} />}
     </div>
   );
 };
